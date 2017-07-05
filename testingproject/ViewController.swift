49fa1abbd987aa560ae3f8645ebd1959098c2e99
CGTPuneet <punit.acharya@cgt.co.in>
2017-07-05 17:35:52 +0530

    Initial Commit

---
+++ testingproject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testingproject
+//
+//  Created by KHALEEL AHAMAD on 05/07/17.
+//  Copyright © 2017 Puneet Acharya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
