b7f7a1068f9986920233d0322a3223beb5bc4451
CGTPuneet <punit.acharya@cgt.co.in>
2017-07-05 17:43:13 +0530

    puneet commit

--- testingproject/ViewController.swift
+++ testingproject/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let a = 0
         // Do any additional setup after loading the view, typically from a nib.
     }
 
---
+++ testingproject/testfile.swift
@@ -0,0 +1,9 @@
+//
+//  testfile.swift
+//  testingproject
+//
+//  Created by KHALEEL AHAMAD on 05/07/17.
+//  Copyright © 2017 Puneet Acharya. All rights reserved.
+//
+
+import Foundation
